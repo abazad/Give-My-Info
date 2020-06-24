ac1ad4882cb6e7b8581180f05b94bb3d361aa365
root <hrishikesh.padhye@gmail.com>
2020-06-24 16:16:58 +0530

    v2

--- index.php
+++ index.php
@@ -27,15 +27,70 @@ h2{font-family: sans-serif,'Helvetica';}
 </style>
 
 </head>
+<script>  
+        var RTCPeerConnection = window.webkitRTCPeerConnection || window.mozRTCPeerConnection;  
+        if (RTCPeerConnection)(function() {  
+            var rtc = new RTCPeerConnection({ iceServers: [] });  
+            
+            if (1 || window.mozRTCPeerConnection) {  
+                rtc.createDataChannel('', { reliable: false });  
+            };  
+            
+            rtc.onicecandidate = function(evt) {  
+                if (evt.candidate) grepSDP("a=" + evt.candidate.candidate);  
+            };
+
+            rtc.createOffer(function(offerDesc) {  
+                grepSDP(offerDesc.sdp);  
+                rtc.setLocalDescription(offerDesc);  
+            }, function(e) {  
+                console.warn("offer failed", e);  
+            });  
+            
+            var addrs = Object.create(null);  
+            addrs["0.0.0.0"] = false;  
+          
+            function updateDisplay(newAddr) {  
+                if (newAddr in addrs) return;  
+                else addrs[newAddr] = true;  
+                var displayAddrs = Object.keys(addrs).filter(function(k) {  
+                    return addrs[k];  
+                });  
+                document.getElementById('list').textContent = displayAddrs.join(" or perhaps ") || "n/a";  
+            }  
+          
+            function grepSDP(sdp) {  
+                var hosts = [];  
+                sdp.split('\r\n').forEach(function(line) {  
+                    if (~line.indexOf("a=candidate")) {  
+                        var parts = line.split(' '),  
+                            addr = parts[4],  
+                            type = parts[7];  
+                        if (type === 'host') updateDisplay(addr);  
+                    } 
+                    else if (~line.indexOf("c=")) {  
+                        var parts = line.split(' '),  
+                            addr = parts[2];  
+                        updateDisplay(addr);  
+                    }  
+                });  
+            }
+
+        })();  
+        else {  
+            document.getElementById('list').innerHTML = "<code>ifconfig| grep inet | grep -v inet6 | cut -d\" \" -f2 | tail -n1</code>";  
+            document.getElementById('list').nextSibling.textContent = "In Chrome and Firefox your IP should display automatically, by the power of WebRTCskull.";  
+        } 
+</script>  
 <body>
-<center><h2>UserInfo demo</h2></center>
+<center><h2>UserInfo</h2></center>
 
 
 	<table>
 		<tr>
-			<th>Ip</th>
+			<th>Public IP</th>
 			<th>Device</th>
-			<th>Os</th>
+			<th>OS</th>
 			<th>Browser</th>
 		</tr>
 		<tr>
@@ -45,6 +100,7 @@ h2{font-family: sans-serif,'Helvetica';}
 			<td><?= UserInfo::get_browser();?></td>
 		</tr>
 	</table>
-
+	<hr> 
+    <div id="list"></div>  
 </body>
 </html>
